36732606d6f2937dac9efec75c350121a53a4a2e
Pete <peteburquette@gmail.com>
2023-09-08 03:35:52 +0100

    app icons, logos

---
+++ app/src/test/java/com/buachaillmaith/blakesorganicandroid/ExampleUnitTest.java
@@ -0,0 +1,16 @@
+package com.buachaillmaith.blakesorganicandroid;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See testing documentation at: http://d.android.com/tools/testing.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
